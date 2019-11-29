1d39fef6376937c519ffe139d56636e05b996b9b
amisl0v11 <amisl0v11@gmail.com>
2019-11-29 11:22:38 +0800

    //Yang say Hello!

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Jt say Hi all
-        //Yang say Hi
+        //Yang say Hello!
     }
 }
