6a9401f7ab62a7e5077f7103c21c7e871cb62cfb
amisl0v11 <amisl0v11@gmail.com>
2019-11-29 11:28:05 +0800

    考試準備好了沒？

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Jt say Hi all
+        //考試準備好了沒？
     }
 }
